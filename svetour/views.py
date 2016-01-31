f15aff89574892a45774466aaf586aed953b7bcc
Your Name <you@example.com>
2016-02-01 00:26:08 +0300

    Andorra

--- svetour/views.py
+++ svetour/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'svetour/home.html', {})
+
+def andorra(request):
+    return render(request, 'svetour/andorra.html', {})
